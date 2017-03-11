f779ea3ce2d2d62e9d1ca9371530e58cec20af99
nrth <foeldes@ionauts.com>
2017-03-11 19:31:53 +0100

    Error handling for missing or not readable user config files.

--- ssb-tool/AccountDiscovery.cs
+++ ssb-tool/AccountDiscovery.cs
@@ -1,4 +1,5 @@
-﻿using SourceSchemaParser.Utilities;
+﻿using Newtonsoft.Json.Linq;
+using SourceSchemaParser.Utilities;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,13 +12,13 @@ namespace ssb_tool
         private String _userdataPath { get; set; }
         private String _localconfigPath = @"\config\localconfig.vdf";
 
-        private Dictionary<String, String> _accounts = 
-            new Dictionary<String, String>();
+        private Dictionary<String, String> _accounts;
 
         public AccountDiscovery(String userdataPath = 
                                 @"C:\Program Files (x86)\Steam\userdata\")
         {
             _userdataPath = userdataPath;
+            _accounts = new Dictionary<String, String>();
         }
 
         public void fetchAccounts()
@@ -29,27 +30,24 @@ namespace ssb_tool
 
             foreach (var dir in accountDirectories)
             {
-                try
+                String path = dir + _localconfigPath;
+
+                if (!File.Exists(path))
                 {
-                    String path = dir + _localconfigPath;
-
-                    dynamic localconfig = VDFConvert.ToJObject(
-                                            File.ReadAllLines(path)
-                                          );
-                    
-                    String personaName = localconfig.UserLocalConfigStore.
-                                         friends.PersonaName.ToString();
-
-                    _accounts.Add(
-                        dir.Split('\\').Last(),
-                        personaName
-                    );
-
-                } catch
-                {
-                    Console.WriteLine("Can not access localconfig"
-                                    + "for account directory " + dir);
+                    continue;
                 }
+
+                JObject localconfig = VDFConvert.ToJObject(
+                                        File.ReadAllLines(path)
+                                        );
+
+                String personaName = localconfig["UserLocalConfigStore"]["friends"]["PersonaName"].ToString();
+
+                _accounts.Add(
+                    dir.Split('\\').Last(),
+                    personaName
+                );
+
             }
         }
 
